0805445eec70f7fbe74c35df290c5b711f498afd
Ivan Rakaric <rakarich.ivan@gmail.com>
2023-07-02 19:38:01 +0200

    prva verzija - ok za predati

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Messages from './components/Messages';
+import Input from './components/Input';
+
+function randomName() {
+	const adjectives = [
+		'White',
+		'Cool',
+		'Spring',
+		'Winter',
+		'Dusk',
+		'Twilight',
+		'Dawn',
+		'Crimson',
+		'Wispy',
+		'Weathered',
+	];
+	const nouns = [
+		'Leaf',
+		'Tree',
+		'Glitter',
+		'Forest',
+		'Hill',
+		'Cloud',
+		'Meadow',
+		'Sun',
+		'Glade',
+		'Bird',
+		'Brook',
+		'Butterfly',
+	];
+	const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+	const noun = nouns[Math.floor(Math.random() * nouns.length)];
+	return `${adjective}  ${noun}`;
+}
+
+function randomColor() {
+	return '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+}
+
+const channelId = '8W5r3AKbbPBackrW';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [messages, setMessages] = useState([]);
+	const [member, setMember] = useState({
+		username: randomName(),
+		color: randomColor(),
+	});
+	const [drone, setDrone] = useState();
+
+	useEffect(() => {
+		const drone = new window.Scaledrone(channelId, {
+			data: member,
+		});
+		setMember((prevMember) => ({
+			...prevMember,
+			id: drone.id,
+		}));
+		setDrone(drone);
+		const room = drone.subscribe('observable-room');
+		room.on('data', (data, member) => {
+			setMessages((prevMessages) => [
+				...prevMessages,
+				{
+					text: data,
+					member: member.clientData,
+				},
+			]);
+		});
+	}, []);
+
+	const onSendMessage = (message) => {
+		drone.publish({
+			room: 'observable-room',
+			message,
+		});
+	};
+	console.log(messages);
+	return (
+		<>
+			<div className='App'>
+				<header className='App-header'>
+					<h1>Chat App</h1>
+				</header>
+				<Messages messages={messages} currentMember={member} />
+				<Input onSendMessage={onSendMessage}></Input>
+			</div>
+		</>
+	);
 }
 
 export default App;
